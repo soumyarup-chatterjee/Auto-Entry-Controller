48c5f19e9d5d81b7be171c1ae760926b097600f1
soumyarup-chatterjee <soumyarupch@gmail.com>
2021-06-26 18:51:14 +0530

    Modifications Part 1, 26th June

---
+++ arduino/temp_sense_program/temp_sense_program.ino
@@ -0,0 +1,19 @@
+
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+void setup() {
+  Serial.begin(9600); 
+
+  mlx.begin();  
+}
+
+void loop() {
+  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC()); 
+  Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
+
+  Serial.println();
+  delay(2000);
+}
